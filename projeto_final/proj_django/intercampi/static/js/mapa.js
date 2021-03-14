fdbecfd70eb2bb9e9e4c844b9eec9d541b696ab4
Thalita Lopes Mascarenhas <thalimascarenhas@gmail.com>
2021-03-14 02:47:20 +0200

    Commit 13022021_9

--- projeto_final/proj_django/intercampi/static/js/mapa.js
+++ projeto_final/proj_django/intercampi/static/js/mapa.js
@@ -82,7 +82,7 @@ mapa.on('click', intervalo);
 function Pararfuncao(){
   clearInterval(intervalo);
   alert('Compartilhamento encerrado!')
-  location.href = 'http://200.17.225.161/index'
+  location.href = 'https://200.17.225.161/index'
   var json = {"id_vg":id_viagem, "nome_linha":nome_linha}
   $.ajax(
     {
